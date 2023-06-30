5ed8d465f0560b76923c0c410ccf498adbabdf33
daniel.borne <daniel.borne@kinsaleins.com>
2023-06-30 09:19:38 -0400

    added java and python lsps

--- .config/nvim/after/plugin/lsp.lua
+++ .config/nvim/after/plugin/lsp.lua
@@ -8,7 +8,12 @@ lsp.ensure_installed({
   'angularls',
   'emmet_ls',
   'cssls',
-  'java_language_server'
+  'jdtls',
+  'jsonls',
+  'html',
+  'pylsp',
+  'flake8',
+  'pylint'
 })
 
 lsp.nvim_workspace()
@@ -22,28 +27,19 @@ local cmp_mappings = lsp.defaults.cmp_mappings({
   ['<C-Space>'] = cmp.mapping.complete(),
 })
 
-
-cmp_mappings['<Tab>'] = nil
-cmp_mappings['<S-Tab>'] = nil
+lsp.set_preferences({
+  sign_icons = {}
+})
 
 lsp.setup_nvim_cmp({
   mapping = cmp_mappings
 })
 
-lsp.set_preferences({
-    suggest_lsp_servers = false,
-    sign_icons = {
-        error = 'E',
-        warn = 'W',
-        hint = 'H',
-        info = 'I'
-    }
-})
-
 lsp.on_attach(function(client, bufnr)
-  local opts = {buffer = bufnr, remap = false}
-
+  print("help")
+  local opts = { buffer = bufnr, remap = false }
   vim.keymap.set("n", "gd", function() vim.lsp.buf.definition() end, opts)
+  vim.keymap.set("n", "gi", function() vim.lsp.buf.declaration() end, opts)
   vim.keymap.set("n", "K", function() vim.lsp.buf.hover() end, opts)
   vim.keymap.set("n", "<leader>vws", function() vim.lsp.buf.workspace_symbol() end, opts)
   vim.keymap.set("n", "<leader>vd", function() vim.diagnostic.open_float() end, opts)
@@ -56,8 +52,3 @@ lsp.on_attach(function(client, bufnr)
 end)
 
 lsp.setup()
-
-vim.diagnostic.config({
-    virtual_text = true
-})
-
