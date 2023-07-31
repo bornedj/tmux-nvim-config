8c276d561e8f37b523c51cc6d8986794884eece3
daniel.borne <daniel.borne@kinsaleins.com>
2023-07-31 13:05:33 -0400

    feat: added command to open file in native windows programs

--- .config/nvim/lua/daniel/remap.lua
+++ .config/nvim/lua/daniel/remap.lua
@@ -4,7 +4,6 @@ vim.keymap.set("i", "<leader>pv", vim.cmd.Ex)
 vim.keymap.set("v", "<leader>pv", vim.cmd.Ex)
 vim.keymap.set("n", "<leader>Pv", "<cmd> Sex! <CR>")
 
-
 vim.keymap.set("v", "J", ":m '>+1<CR>gv=gv")
 vim.keymap.set("v", "K", ":m '<-2<CR>gv=gv")
 
@@ -37,6 +36,16 @@ vim.keymap.set("n", "<leader>j", "<cmd>lprev<CR>zz")
 
 vim.keymap.set("n", "<leader><leader>", function()
     vim.cmd("so")
+end)
+
+-- command to open a file with explorer.exe so I can open files with their native
+-- windows default program. I.E. open html files in windows chrome
+vim.keymap.set("n", "<leader>fp", function()
+    local startingDir = vim.fn.getcwd()
+    local explorer = string.format("!explorer.exe %s", vim.fn.expand('%:t'))
+    vim.cmd("cd %:h")
+    vim.cmd(explorer)
+    vim.cmd(string.format("cd %s", startingDir))
 end)
 
 vim.keymap.set("n", "<leader>s", [[:%s/\<<C-r><C-w>\>/<C-r><C-w>/gI<Left><Left><Left>]])
