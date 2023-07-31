ee3358d9a74e111247c8338cccf21ec68229d34a
daniel.borne <daniel.borne@kinsaleins.com>
2023-07-31 15:50:43 -0400

    feat added java runtimes

--- .config/nvim/ftplugin/java.lua
+++ .config/nvim/ftplugin/java.lua
@@ -1,21 +1,24 @@
+local project_name = vim.fn.fnamemodify(vim.fn.getcwd(), ':p:h:t')
+local workspace_dir = '/mnt/c/Users/daniel.borne/gitlab_repos/' .. project_name
+
 local config = {
-    cmd = { '/usr/lib/jdtls/bin/jdtls' },
-    root_dir = vim.fs.dirname(vim.fs.find({ 'gradlew', '.git', 'mvnw' }, { upward = true })[1]),
+    cmd = {
+        'java',
+        '-configuration', '/usr/lib/jdtls/bin/jdtls',
+        '-data', workspace_dir
+    },
+    root_dir = vim.fs.dirname(vim.fs.find({ 'gradlew', '.git', 'mvnw', 'pom' }, { upward = true })[1]),
     settings = {
         java = {
             configuration = {
                 runtimes = {
-                    -- {
-                    --     name = "JavaSE-8",
-                    --     path = "/usr/lib/jvm/java-8-openjdk-amd64/bin/java"
-                    -- },
-                    -- {
-                    --     name = "JavaSE-17",
-                    --     path = "/usr/lib/jvm/java-17-openjdk-amd64/bin/java"
-                    -- },
+                    {
+                        name = "JavaSE-8",
+                        path = "/usr/lib/jvm/java-8-openjdk-amd64/"
+                    },
                     {
                         name = "JavaSE-17",
-                        path = "/usr/lib/jvm/openjdk-17/"
+                        path = "/usr/lib/jvm/java-17-openjdk-amd64/"
                     },
                 }
             }
