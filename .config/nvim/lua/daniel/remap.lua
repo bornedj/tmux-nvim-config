ec87b6275a12fc745b3727bd3cedb6ed1cf7dff5
daniel.borne <daniel.borne@kinsaleins.com>
2023-09-19 10:55:47 -0400

    removed unneccessary extra toggle

--- .config/nvim/lua/daniel/remap.lua
+++ .config/nvim/lua/daniel/remap.lua
@@ -83,10 +83,8 @@ end)
 vim.keymap.set("n", "<leader>s", [[:%s/\<<C-r><C-w>\>/<C-r><C-w>/gI<Left><Left><Left>]])
 vim.keymap.set("n", "<leader>x", "<cmd>!chmod +x %<CR>", { silent = true })
 
-vim.keymap.set("n", "<leader>hn", function()
-    vim.cmd("set number relativenumber")
-end)
-vim.keymap.set("n", "<leader>an", function()
+-- line number toggles
+vim.keymap.set("n", "<leader>ln", function()
     vim.cmd("set number")
     vim.cmd("set rnu!")
 end)
